eafaf5275df0ec49cf23a407b80db0b709ea7982
mariangelamarasciuolo <130611642+mariangelamarasciuolo@users.noreply.github.com>
2023-10-13 15:57:27 +0200

    Operazione
    Ricerca per anno

--- untitled/src/main/java/mariangelamarasciuolo/Application.java
+++ untitled/src/main/java/mariangelamarasciuolo/Application.java
@@ -3,17 +3,24 @@ package mariangelamarasciuolo;
 public class Application {
 
     public static void main(String[] args) {
-        Libro libro1 = new Libro("545441", "libro bello", 2023, 2000, "Fantasy", "Peppino Magnarone");
-        //Libro libro2 = new Libro("654652", "libro stupendo", 2021, 1500, "Romantico", "Mario Pappolla");
+        Libro libro1 = new Libro("545441", "libro bello", 2022, 2000, "Fantasy", "Peppino Magnarone");
+        Libro libro2 = new Libro("654652", "libro stupendo", 2021, 1500, "Romantico", "Mario Pappolla");
 
-        Riviste riviste1 = new Riviste("445454", "Alieni", 2023, 200, Periodicita.MENSILE);
-        //Riviste riviste2 = new Riviste("565476", "Vichinghi", 2023, 150, Periodicita.TREMESTRALE);
+        Riviste riviste1 = new Riviste("445454", "Alieni", 2022, 200, Periodicita.MENSILE);
+        Riviste riviste2 = new Riviste("565476", "Vichinghi", 2023, 150, Periodicita.TREMESTRALE);
 
-        Archivio archivio = new Archivio();
-        archivio.aggiungiElemento(libro1);
-        archivio.aggiungiElemento(riviste1);
+        System.out.println("-----Aggiungi elemento-----");
+        Archivio.aggiungiElemento(libro1);
+        Archivio.aggiungiElemento(riviste1);
 
-        archivio.rimuoviElemento(libro1.getIsbn());
-        archivio.rimuoviElemento(riviste1.getIsbn());
+        System.out.println("-----Ricerca per anno-----");
+        Archivio.ricercaAnnoPubblicazione(2022);
+
+
+        System.out.println("-----Elimina elemento-----");
+        Archivio.rimuoviElemento(libro1.getIsbn());
+        Archivio.rimuoviElemento(riviste1.getIsbn());
+
+
     }
 }
--- untitled/src/main/java/mariangelamarasciuolo/Archivio.java
+++ untitled/src/main/java/mariangelamarasciuolo/Archivio.java
@@ -1,21 +1,27 @@
 package mariangelamarasciuolo;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
 ;
 
-public class Archivio {
-    private Map<String, Catalogo> remember = new HashMap<>();
+public abstract class Archivio {
+    static Map<String, Catalogo> remember = new HashMap<>();
 
-    public void aggiungiElemento(Catalogo cat) {
+    public static void aggiungiElemento(Catalogo cat) {
         remember.put(cat.getIsbn(), cat);
         System.out.println("Elemento aggiunto Correttamente");
     }
 
-    public void rimuoviElemento(String codiceIsbn) {
+    public static void rimuoviElemento(String codiceIsbn) {
         remember.remove(codiceIsbn);
         System.out.println("Eliminato correttamente");
     }
-    
+
+    public static void ricercaAnnoPubblicazione(int annoPubb) {
+        Collection<Catalogo> listaCatalogo = remember.values();
+        Map<Integer, List<Catalogo>> listaProvvisoria = listaCatalogo.stream().collect(Collectors.groupingBy(Catalogo::getAnnoPubb));
+        List<Catalogo> listaAnno = new ArrayList<>(listaProvvisoria.get(annoPubb));
+        System.out.println(listaAnno);
+    }
 }
